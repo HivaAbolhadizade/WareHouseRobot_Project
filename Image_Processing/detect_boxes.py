759795a603c13a4667d63c3e5d963673aa22fdae
Amirhossein Abolhasani <46609807+AmirAAZ818@users.noreply.github.com>
2023-12-21 09:53:14 +0330

    Update detect_boxes.py

--- Image_Processing/detect_boxes.py
+++ Image_Processing/detect_boxes.py
@@ -29,11 +29,15 @@ def detect_box(frame, upper_hsv, lower_hsv, erode, dilate, num_box=1):
     """
     output = None
     # resizing the frame so that it would be easier to handle the image and also lowers the computation.
+
     frame = imutils.resize(frame, width=600)
+
     # reducing the details in the frame.
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+
     # converting the RGB color space of the image (frame) to HSV color space
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
     # color thresholding in hsv system
     mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
     """  
@@ -52,15 +56,17 @@ def detect_box(frame, upper_hsv, lower_hsv, erode, dilate, num_box=1):
     cv2.imwrite(
         r'E:\University of Kerman\Term 5\Computer Architecture\Project\Robot_Project\Image_Processing\test\mask.png',
         mask)
+
     # finding contours of the processed frame
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
     # using imutils to make sure we have pure contours and nothing else with it.
     cnts = imutils.grab_contours(cnts)
+
     # if we've had any contours in the picture,
     # we will find the center of the ball and return it as output, else we return None as output
     if len(cnts) > 0:
         # c is a contour that hase the biggest area among all contours that we found in the frame.
-        # c1 = max(cnts, key=cv2.contourArea)
         sorted_cnts = sorted(cnts, key=cv2.contourArea)
 
         # finding the smallest enclosing circle
@@ -76,9 +82,6 @@ def detect_box(frame, upper_hsv, lower_hsv, erode, dilate, num_box=1):
         cv2.destroyAllWindows()
         output = centers
 
-        # cv2.imwrite(
-        #         r'E:\University of Kerman\Term 5\Computer Architecture\Project\Robot_Project\Image_Processing\test',
-        #         frame)
         return output, False
 
 
