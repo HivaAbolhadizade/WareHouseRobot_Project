7ea6536401c1ff871dd2b951bac56c846e6b6d40
Amirhossein Abolhasani <46609807+AmirAAZ818@users.noreply.github.com>
2024-01-25 15:54:51 +0330

    codes

--- Raspi_Programs/movement_remote.py
+++ Raspi_Programs/movement_remote.py
@@ -1,19 +1,23 @@
 # Example Python code to handle commands
 from flask import Flask
 
+import Movement_v2 as cmd
+
+
 def f():
     print("forward")
+
 
 def b():
     print('backward')
 
+
 def r():
     print('right')
 
+
 def l():
     print('left')
-
-
 
 
 app = Flask(__name__)
@@ -21,19 +25,32 @@ app = Flask(__name__)
 @app.route('/<command>')
 def control_robot(command):
     # Implement logic to control the robot based on the received command
+
     if command == 'move_forward':
         # Code to move the robot forward
-        f()
+        cmd.forward()
+        # print('f')
     elif command == 'move_backward':
         # Code to move the robot backward
-        b()
+        cmd.backward()
+        # print('b')
     elif command == 'move_left':
         # Code to turn the robot left
-        l()
+        cmd.turn_left()
+        # print('l')
     elif command == 'move_right':
         # Code to turn the robot right
-        r()
+        # print('r')
+        cmd.turn_right()
+    elif command == 'catch':
+        # print('c')
+        cmd.close_catcher()
+    elif command == 'drop':
+        cmd.open_catcher()
+        # print('d')
+
     return 'Command receive'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
